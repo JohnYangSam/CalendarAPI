1d75840d619d10a60ec0df60ff90193131a53ecd
TP <tp@plannary.com>
2012-03-03 18:31:57 -0800

    configure for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,8 @@ CalendarAPI::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
-
+  config.assets.initialize_on_precompile = false
+  
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
