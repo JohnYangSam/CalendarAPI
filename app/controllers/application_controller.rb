d2ada15f65d75aee5e3344139cdc5cd79303d513
TP <tp@plannary.com>
2012-03-04 18:32:59 -0800

    Cleaned up a few hacks and bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     [:sd, :st, :sz, :ey, :em, :ed, :et, :ez].each do |k|
       next if [:sz, :ez].include?(k) && !(params[k] =~ /[+-]\d{2}:\d{2}/)
       if params[k]
-        opts[k] = params[:k]
+        opts[k] = params[k]
       end
     end
     
--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -6,23 +6,26 @@ class CalendarController < ApplicationController
 
     @title = opts[:tt]
     @url = googlecalendar_url opts
-    @datestring = Time.new(opts[:sy],opts[:sm], opts[:sd]).strftime("5:00pm, %A, %B %C, %Y ")
+    @datestring = Time.new(opts[:sy],opts[:sm], opts[:sd]).strftime("%A, %B %C, %Y ")
+    
+    @datestring = params[:st] + ", "+ @datestring if params[:st]
+      
     
     render layout: false
   end
   
   def embed 
     opts = get_params
-    
-    @url = popup_url opts
+    @url = popup_url(opts)
   end
   
+  # http://www.google.com/googlecalendar/event_publisher_guide_detail.html
   def googlecalendar
     sy = params[:sy].to_i
     sm = params[:sm].to_i
     sd = params[:sd]
     st = params[:st]
-    sz = params[:sz] if params[:sz] =~ /[+-]\d{2}:\d{2}/
+    sz = params[:sz] =~ /[+-]\d{2}:\d{2}/ ? params[:sz] : Time.now.strftime("%:z")
     
     ey = params[:ey]
     em = params[:em]
@@ -30,35 +33,33 @@ class CalendarController < ApplicationController
     et = params[:et]
     ez = params[:ez]
     
-    if !st.blank? && st =~ /\d{0,2}[:.]\d{2}/
-      st = st.scan /\d+/
-      sh = st.first.to_i + (st.include?('p') ? 12 : 0)
-      sm = st.last.to_i
-      Time.new(sy, sm, sd, sh, sm, nil, sz)
-      date = Time.now.getutc.strftime('%Y%m%d')
-    else
-      Time.new(sy, sm, sd, nil, nil, nil, sz)
-      date = Time.now.getutc.strftime('%Y%m%d')
-    end
     
     # e.g. 20060415T180000Z
-    date = Time.now.getutc.strftime('%Y%m%d')
-
-    if !ey.blank?
-      date += "/#{Time.now.getutc.strftime('%Y%m%d')}"
+    if !st.blank? && st =~ /\d{0,2}[:.]\d{2}/
+      sta = st.scan /\d+/
+      sth = sta.first.to_i + (st.include?('p') ? 12 : 0)
+      stm = sta.last.to_i
+      
+      # timezones are kinda messed up when crossing dst
+      date = Time.new(sy, sm, sd, sth, stm, nil, sz).getutc.strftime('%Y%m%dT%H%M%SZ')
     else
-      date += "/#{date}"
+      date = Time.new(sy, sm, sd, nil, nil, nil, sz).getutc.strftime('%Y%m%d')
     end
+
+    # if !ey.blank?
+      # date += "/#{Time.now.getutc.strftime('%Y%m%d')}"
+    # else
+      date += "/#{date}"
+    # end
     
     options = {
       action:  'TEMPLATE',
       text:    params[:tt] || "New Event",
-      dates:   date,
-      sprop:   'foo'
+      dates:   date
     }
     
     opt = options.collect {|k,v| "#{CGI.escape k.to_s}=#{CGI.escape v}"}.join '&'
-    # render :text => options.inspect
+    # render :text => options.inspect and return
     redirect_to "http://www.google.com/calendar/event?" + opt
   end
 
